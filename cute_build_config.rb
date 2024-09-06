613506962f9f1eabad4525c551872bbef07a589a
Piotr Usewicz <piotr@layer22.com>
2024-09-06 17:39:06 +0200

    Use newer hook name

--- cute_build_config.rb
+++ cute_build_config.rb
@@ -6,14 +6,7 @@ MRuby::Build.new do |conf|
 
   if ENV['DEBUG'] == 'true'
     conf.enable_debug
-    conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
     conf.gem core: 'mruby-bin-debugger'
-
-    # Debug flags
-    conf.cc.flags << "-g3"
-    conf.cc.flags << "-Wno-unused-parameter"
-    conf.cc.flags << "-Wno-unused-variable"
-    conf.cc.flags << "-Wno-unused-function"
-    conf.cc.flags << "-Wno-unused-value"
+    conf.cc.defines = %w(MRB_USE_DEBUG_HOOK)
   end
 end
