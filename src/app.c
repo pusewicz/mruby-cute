502f20241744a42b820fcd428e334129c7005d7c
Piotr Usewicz <piotr@layer22.com>
2025-03-31 11:58:27 +0200

    Add support for specifying a callback

--- src/app.c
+++ src/app.c
@@ -30,10 +30,41 @@ static mrb_value mrb_cf_app_destroy(mrb_state* mrb, mrb_value self)
     return mrb_nil_value();
 }
 
+typedef struct mrb_cf_callback_data {
+    mrb_state* mrb;
+    mrb_value callback;
+} mrb_cf_callback_data;
+
+void mrb_cf_app_update_callback_function(void* ptr)
+{
+    mrb_cf_callback_data* data = (mrb_cf_callback_data*)ptr;
+    mrb_state* mrb = data->mrb;
+    mrb_value callback = data->callback;
+
+    // Call the Ruby callback
+    mrb_funcall(mrb, callback, "call", 0);
+}
+
 static mrb_value mrb_cf_app_update(mrb_state* mrb, mrb_value self)
 {
-    // TODO: Add support for the OnUpdate callback.
-    cf_app_update(NULL);
+    mrb_value on_update_callback = mrb_nil_value();
+
+    mrb_get_args(mrb, "|&", &on_update_callback);
+
+    if (mrb_nil_p(on_update_callback)) {
+        // No callback provided, use the default update function
+        cf_app_update(NULL);
+    } else {
+        // Wrap the callback in a struct to pass it to the C function
+        mrb_cf_callback_data data;
+
+        data.mrb = mrb;
+        data.callback = on_update_callback;
+
+        cf_set_update_udata(&data);
+        cf_app_update(mrb_cf_app_update_callback_function);
+    }
+
     return mrb_nil_value();
 }
 
@@ -59,7 +90,7 @@ void mrb_cute_app_init(mrb_state* mrb, struct RClass* mCute)
     mrb_define_module_function(mrb, mCute, "cf_make_app", mrb_cf_make_app, MRB_ARGS_REQ(7));
     mrb_define_module_function(mrb, mCute, "cf_app_is_running", mrb_cf_app_is_running, MRB_ARGS_NONE());
     mrb_define_module_function(mrb, mCute, "cf_app_destroy", mrb_cf_app_destroy, MRB_ARGS_NONE());
-    mrb_define_module_function(mrb, mCute, "cf_app_update", mrb_cf_app_update, MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mCute, "cf_app_update", mrb_cf_app_update, MRB_ARGS_OPT(1));
     mrb_define_module_function(mrb, mCute, "cf_default_display", mrb_cf_default_display, MRB_ARGS_NONE());
     mrb_define_module_function(mrb, mCute, "cf_app_draw_onto_screen", mrb_cf_app_draw_onto_screen, MRB_ARGS_OPT(1));
 }
