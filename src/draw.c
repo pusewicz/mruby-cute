b4b53a2fa506b8d0f33d3d87620bdc57dec36e2b
Piotr Usewicz <piotr@layer22.com>
2025-04-03 13:54:42 +0200

    Add cf_draw_translate and cf_draw_translate_v2 bindings
    This commit adds implementations for:
    - cf_draw_translate: Translates the current coordinate system by x and y
    - cf_draw_translate_v2: Translates the current coordinate system by a vector
    
    It also adds corresponding test cases to verify the functions work correctly.
    These bindings are part of the missing transform functions in issue #6.

--- src/draw.c
+++ src/draw.c
@@ -49,6 +49,27 @@ static mrb_value mrb_cf_draw_scale_v2(mrb_state* mrb, mrb_value self)
     return mrb_nil_value();
 }
 
+static mrb_value mrb_cf_draw_translate(mrb_state* mrb, mrb_value self)
+{
+    mrb_float x, y;
+
+    mrb_get_args(mrb, "ff", &x, &y);
+    cf_draw_translate((float)x, (float)y);
+
+    return mrb_nil_value();
+}
+
+static mrb_value mrb_cf_draw_translate_v2(mrb_state* mrb, mrb_value self)
+{
+    mrb_value obj;
+
+    mrb_get_args(mrb, "o", &obj);
+
+    cf_draw_translate_v2(*mrb_cf_v2_unwrap(mrb, obj));
+
+    return mrb_nil_value();
+}
+
 static mrb_value mrb_cf_draw_rotate(mrb_state* mrb, mrb_value self)
 {
     mrb_float radians;
@@ -110,6 +131,8 @@ void mrb_cute_draw_init(mrb_state* mrb, struct RClass* mCute)
     mrb_define_module_function(mrb, mCute, "cf_draw_sprite", mrb_cf_draw_sprite, MRB_ARGS_REQ(1));
     mrb_define_module_function(mrb, mCute, "cf_draw_scale", mrb_cf_draw_scale, MRB_ARGS_REQ(2));
     mrb_define_module_function(mrb, mCute, "cf_draw_scale_v2", mrb_cf_draw_scale_v2, MRB_ARGS_REQ(1));
+    mrb_define_module_function(mrb, mCute, "cf_draw_translate", mrb_cf_draw_translate, MRB_ARGS_REQ(2));
+    mrb_define_module_function(mrb, mCute, "cf_draw_translate_v2", mrb_cf_draw_translate_v2, MRB_ARGS_REQ(1));
     mrb_define_module_function(mrb, mCute, "cf_draw_rotate", mrb_cf_draw_rotate, MRB_ARGS_REQ(1));
     mrb_define_module_function(mrb, mCute, "cf_draw_push", mrb_cf_draw_push, MRB_ARGS_NONE());
     mrb_define_module_function(mrb, mCute, "cf_draw_pop", mrb_cf_draw_pop, MRB_ARGS_NONE());
--- test/draw_test.rb
+++ test/draw_test.rb
@@ -50,42 +50,32 @@ assert('Cute::cf_draw_quad2') do
   end
 end
 
-assert('Cute::cf_draw_quad') do
-  within_app do
-    # Create min and max points for AABB
-    min = Cute::V2.new(10, 10)
-    max = Cute::V2.new(100, 100)
-
-    # Create an AABB
-    bb = Cute::Aabb.new(min, max)
-
-    # This should not raise an error
-    assert_nothing_raised do
-      Cute::cf_draw_quad(bb, 2.0, 0.0)
-    end
-  end
-end
-
-assert('Cute::cf_draw_quad2') do
-  within_app do
-    # Create four points for quad
-    p0 = Cute::V2.new(10, 10)
-    p1 = Cute::V2.new(100, 10)
-    p2 = Cute::V2.new(100, 100)
-    p3 = Cute::V2.new(10, 100)
-
-    # This should not raise an error
-    assert_nothing_raised do
-      Cute::cf_draw_quad2(p0, p1, p2, p3, 2.0, 0.0)
-    end
-  end
-end
-
 assert('Cute::cf_draw_rotate') do
   within_app do
     # This should not raise an error
     assert_nothing_raised do
       Cute::cf_draw_rotate(Math::PI / 4.0)  # 45 degrees in radians
+    end
+  end
+end
+
+assert('Cute::cf_draw_translate') do
+  within_app do
+    # This should not raise an error
+    assert_nothing_raised do
+      Cute::cf_draw_translate(50.0, 75.0)
+    end
+  end
+end
+
+assert('Cute::cf_draw_translate_v2') do
+  within_app do
+    # Create a vector for translation
+    position = Cute::V2.new(50.0, 75.0)
+    
+    # This should not raise an error
+    assert_nothing_raised do
+      Cute::cf_draw_translate_v2(position)
     end
   end
 end
