b707c189bf08b6952117c5983c3f3c653ad6e7e2
Piotr Usewicz <piotr@layer22.com>
2025-04-03 23:02:57 +0200

    Build flags

--- build_config.rb
+++ build_config.rb
@@ -1,16 +1,17 @@
 MRuby::Build.new do |conf|
   toolchain :clang
-  enable_debug
 
   conf.gembox 'full-core'
+
   conf.gem File.expand_path(File.dirname(__FILE__))
   conf.gem core: "mruby-sprintf" # Not sure why this is necessary as it's already in the full-core gembox
-  conf.enable_test
+
   conf.compilers.each do |c|
     c.defines += %w(MRB_UTF8_STRING)
   end
 
   if ENV['DEBUG'] == 'true'
+    conf.enable_test
     conf.enable_debug
     conf.compilers.each do |c|
       c.defines += %w(MRB_USE_DEBUG_HOOK)
@@ -18,6 +19,12 @@ MRuby::Build.new do |conf|
       c.flags << "-g"
       c.flags << "-g3"
       c.flags << "-glldb"
+    end
+  elsif ENV['RELEASE'] == 'true'
+    # Release flags
+    spec.compilers.each do |c|
+      c.flags << "-O3"
+      c.flags << "-DNDEBUG"
     end
   end
 end
