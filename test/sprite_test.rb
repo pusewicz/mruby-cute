62a481316f2cd698d2fb818a1961beb84b397ba9
Piotr Usewicz <piotr@layer22.com>
2025-04-04 18:12:10 +0200

    Wrap tests in within_app
    This allows for faster test execution as we don't tear down the app for each test

--- test/sprite_test.rb
+++ test/sprite_test.rb
@@ -6,65 +6,63 @@ def within_app(&block)
   Cute.cf_app_destroy
 end
 
-assert("Cute::Sprite#initialize") do
-  sprite = Cute::Sprite.new
-  assert_kind_of(Cute::Sprite, sprite)
-end
+within_app do
+  assert("Cute::Sprite#initialize") do
+    sprite = Cute::Sprite.new
+    assert_kind_of(Cute::Sprite, sprite)
+  end
 
-assert("Cute::Sprite#w") do
-  sprite = Cute::Sprite.new
-  assert_equal(0, sprite.w)
-end
+  assert("Cute::Sprite#w") do
+    sprite = Cute::Sprite.new
+    assert_equal(0, sprite.w)
+  end
 
-assert("Cute::Sprite#h") do
-  sprite = Cute::Sprite.new
-  assert_equal(0, sprite.h)
-end
+  assert("Cute::Sprite#h") do
+    sprite = Cute::Sprite.new
+    assert_equal(0, sprite.h)
+  end
 
-assert("Cute::Sprite#opacity") do
-  sprite = Cute::Sprite.new
-  sprite.opacity = 0.5
-  assert_equal(0.5, sprite.opacity)
-end
+  assert("Cute::Sprite#opacity") do
+    sprite = Cute::Sprite.new
+    sprite.opacity = 0.5
+    assert_equal(0.5, sprite.opacity)
+  end
 
-assert("Cute::Sprite#scale_x") do
-  sprite = Cute::Sprite.new
-  sprite.scale_x = 1.5
-  assert_equal(1.5, sprite.scale_x)
-end
+  assert("Cute::Sprite#scale_x") do
+    sprite = Cute::Sprite.new
+    sprite.scale_x = 1.5
+    assert_equal(1.5, sprite.scale_x)
+  end
 
-assert("Cute::Sprite#scale_y") do
-  sprite = Cute::Sprite.new
-  sprite.scale_y = 1.5
-  assert_equal(1.5, sprite.scale_y)
-end
+  assert("Cute::Sprite#scale_y") do
+    sprite = Cute::Sprite.new
+    sprite.scale_y = 1.5
+    assert_equal(1.5, sprite.scale_y)
+  end
 
-assert("Cute::Sprite#reset") do
-  within_app do
+  assert("Cute::Sprite#reset") do
     sprite = Cute.cf_make_demo_sprite
     assert_nil(sprite.reset)
   end
-end
 
-assert("Cute::Sprite#inspect") do
-  sprite = Cute::Sprite.new
-  assert_match('#<Cute::Sprite:0x* name:"" w:0 h:0 opacity:1.0>', sprite.inspect)
-end
+  assert("Cute::Sprite#inspect") do
+    sprite = Cute::Sprite.new
+    assert_match('#<Cute::Sprite:0x* name:"" w:0 h:0 opacity:1.0>', sprite.inspect)
+  end
 
-assert("Cute::Sprite#draw") do
-  within_app do
+  assert("Cute::Sprite#draw") do
     sprite = Cute.cf_make_demo_sprite
     sprite.play("spin")
     sprite.update
     assert_nil(sprite.draw)
   end
-end
 
-assert("Cute::Sprite#loop?") do
-  sprite = Cute::Sprite.new
-  assert_true(sprite.loop?)
-  sprite.loop = false
-  assert_false(sprite.loop?)
-  sprite.loop = true
-  assert_true(sprite.loop?)
+  assert("Cute::Sprite#loop?") do
+    sprite = Cute::Sprite.new
+    assert_true(sprite.loop?)
+    sprite.loop = false
+    assert_false(sprite.loop?)
+    sprite.loop = true
+    assert_true(sprite.loop?)
+  end
 end
